0950d01abda6b75461207ae645d150e3755e86fd
Chloe Chan <chc70@sfu.ca>
2019-05-27 19:15:43 -0700

    wanna die

--- public/calculate.js
+++ public/calculate.js
@@ -18,74 +18,17 @@ function setBoundaries(event)
 	var cm = document.forms["gradeForm"]["CM"].value;
 	var d = document.forms["gradeForm"]["D"].value;
 	var f = document.forms["gradeForm"]["F"].value;
-	//var bounds = document.getElementById("gradeForm");
-	/*var max = bounds.elements["MAX"].value;
-	var ap = bounds.elements["AP"].value;
-	var a = bounds.elements["A"].value;
-	var am = bounds.elements["AM"].value;
-	var bp = bounds.elements["BP"].value;
-	var b = bounds.elements["B"].value;
-	var bm = bounds.elements["BM"].value;
-	var cp = bounds.elements["CP"].value;
-	var c = bounds.elements["C"].value;
-	var cm = bounds.elements["CM"].value;
-	var d = bounds.elements["D"].value;
-	var f = bounds.elements["F"].value;
-	
+
 	var boundsArr = [max,ap,a,am,bp,b,bm,cp,c,cm,d,f];
-	for (i=0; i<11; i++)
+	for (var i=0; i<11; i++)
 	{
-		if (boundsArr[i] <= boundsArr[i+1])
+		if (boundsArr[i+1] >= boundsArr[i])
 		{
 			alert("input overlapped!");
 			return false;
 		}
-	}*/
-	/*if (max <= ap){ 
-		alert("input overlapped!");
-		return false;
-		}
-	if (ap <= a){
-		alert("input overlapped!");
-		return false;
-		}
-	if (a <= am){
-		alert("input overlapped!");
-		return false;
-		}
-	if (am <= bp){
-		alert("input overlapped!");
-		return false;
-		}
-	if (bp <= b){
-		alert("input overlapped!");
-		return false;
-		}
-	if (b <= bm){
-		alert("input overlapped!");
-		return false;
-		}
-	if (bm <= cp){
-		alert("input overlapped!");
-		return false;
-		}
-	if (cp <= c){
-		alert("input overlapped!");
-		return false;
-		}
-	if (c <= cm){
-		alert("input overlapped!");
-		return false;
-		}
-	if (cm <= d){
-		alert("input overlapped!");
-		return false;
-		}
-	if (d <= f){
-		alert("input overlapped!");
-		return false;
-		}
-		*/
+	}
+
 	document.getElementById("grade1").innerHTML = print(counter(max,ap));
 	document.getElementById("grade2").innerHTML = print(counter(ap,a));
 	document.getElementById("grade3").innerHTML = print(counter(a,am));
@@ -97,18 +40,7 @@ function setBoundaries(event)
 	document.getElementById("grade9").innerHTML = print(counter(c,cm));
 	document.getElementById("grade10").innerHTML = print(counter(cm,d));
 	document.getElementById("grade11").innerHTML = print(counter(d,f));
-	/*var numAP = counter(max,ap);
-	var numA = counter(ap,a);
-	var numAM = counter(a,am);
-	var numBP = counter(am,bp);
-	var numB = counter(bp,b);
-	var numBM = counter(b,bm);
-	var numCP = counter(bm,cp);
-	var numC = counter(cp,c);
-	var numCM = counter(c,cm);
-	var numD = counter(cm,d);
-	var numF= counter(d,f);
-	*/
+
 	event.preventDefault();
 }
 
@@ -120,9 +52,11 @@ function counter(bound1, bound2){
 	return count;
 }
 
+
 function print(amount){
 	var txt = "";
 	for (var j=0; j<amount; j++)
 		txt += "☺";
 	return txt;
 }
+	
