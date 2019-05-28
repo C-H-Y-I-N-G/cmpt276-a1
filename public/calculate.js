0741df9c15e738d562a08cf9865cb077e77c0def
Chloe Chan <chc70@sfu.ca>
2019-05-27 17:02:18 -0700

    table updated

--- public/calculate.js
+++ public/calculate.js
@@ -1,10 +1,23 @@
+
 var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03, 49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
 
 
 function setBoundaries()
 {
-	var bounds = document.getElementById("gradeForm");
-	var max = bounds.elements["MAX"].value;
+	var max = document.forms["gradeForm"]["MAX"].value;
+	var ap = document.forms["gradeForm"]["AP"].value;
+	var a = document.forms["gradeForm"]["A"].value;
+	var am = document.forms["gradeForm"]["AM"].value;
+	var bp = document.forms["gradeForm"]["BP"].value;
+	var b = document.forms["gradeForm"]["B"].value;
+	var bm = document.forms["gradeForm"]["BM"].value;
+	var cp = document.forms["gradeForm"]["CP"].value;
+	var c = document.forms["gradeForm"]["C"].value;
+	var cm = document.forms["gradeForm"]["CM"].value;
+	var d = document.forms["gradeForm"]["D"].value;
+	var f = document.forms["gradeForm"]["F"].value;
+	//var bounds = document.getElementById("gradeForm");
+	/*var max = bounds.elements["MAX"].value;
 	var ap = bounds.elements["AP"].value;
 	var a = bounds.elements["A"].value;
 	var am = bounds.elements["AM"].value;
@@ -16,14 +29,16 @@ function setBoundaries()
 	var cm = bounds.elements["CM"].value;
 	var d = bounds.elements["D"].value;
 	var f = bounds.elements["F"].value;
-	var boundsArr = [max,ap,a,am,bp,b,bm,cp,c,cm,d,f];
-	for (i=0; i<boundsArr.length-1; i++)
+	*/
+	/*var boundsArr = [max,ap,a,am,bp,b,bm,cp,c,cm,d,f];
+	for (i=0; i<11; i++)
 	{
 		if (boundsArr[i] <= boundsArr[i+1])
 		{
 			alert("input overlapped!");
 			return false;
-	}
+		}
+	}*/
 	/*if (max <= ap){ 
 		alert("input overlapped!");
 		return false;
